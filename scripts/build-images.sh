680f4ecce1c79e6855d8807bf733d7c9beac5bf7
FazioNico <contact@nicolasfazio.ch>
2018-02-06 17:57:26 +0100

    Update: add build-images.sh

---
+++ scripts/build-images.sh
@@ -0,0 +1,19 @@
+# defin project rootDirectory
+rootDirectory=$(pwd)
+# do loop for each microservice found into packages folder
+for dir in $(ls -d  packages/*); do
+  # go to microservice folder
+  cd $dir
+  # get service name
+  servicename=$(pwd | sed 's#.*/##');
+  # tag image
+  TAG=0.1.$CIRCLE_BUILD_NUM
+  # build docker image
+  docker build -t registry.agenda.ch/nfz_test/$servicename:$TAG  .
+  # login to docker hub
+  docker login registry.agenda.ch -u $USER_DOCKER -p $PASS_DOCKER
+  # push docker image
+  docker push registry.agenda.ch/nfz_test/servicename:$TAG
+  # return to rootDirectory project
+  cd $rootDirectory
+done
